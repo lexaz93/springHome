a6de1d9952048c8cc5c19e1ab343c69fd89e7fc6
lexaz93 <lexaz93@mail.ru>
2020-09-30 22:47:54 +0400

    AOP Logger

---
+++ eviscerator-part-1/src/main/java/quoters/TerminatorQuoter.java
@@ -0,0 +1,33 @@
+package quoters;
+
+import javax.annotation.PostConstruct;
+
+@Profiling
+public class TerminatorQuoter implements Quoter {
+    @InjectRandomInt(min = 2, max = 7)
+    private int repeat;
+    private String message;
+
+    @PostConstruct
+    public void init() {
+        System.out.println("Phase 2");
+        System.out.println(repeat);
+    }
+
+    public TerminatorQuoter() {
+        System.out.println("Phase 1");
+
+    }
+
+    @Override
+    @PostProxy
+    public void sayQuote() {
+        for (int i = 0; i < repeat; i++) {
+            System.out.println("message = " + message);
+        }
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
