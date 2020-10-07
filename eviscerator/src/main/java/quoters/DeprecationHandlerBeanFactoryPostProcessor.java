cbd6606dba83bbb1ba2b37e95b4ed876a829574b
lexaz93 <lexaz93@mail.ru>
2020-10-07 14:10:33 +0400

    Только потрошитель)

---
+++ eviscerator/src/main/java/quoters/DeprecationHandlerBeanFactoryPostProcessor.java
@@ -0,0 +1,28 @@
+package quoters;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
+
+import java.lang.annotation.Annotation;
+
+public class DeprecationHandlerBeanFactoryPostProcessor implements BeanFactoryPostProcessor {
+    @Override
+    public void postProcessBeanFactory(ConfigurableListableBeanFactory configurableListableBeanFactory) throws BeansException {
+        String[] names = configurableListableBeanFactory.getBeanDefinitionNames();
+        for (String name : names) {
+            BeanDefinition beanDefinition = configurableListableBeanFactory.getBeanDefinition(name);
+            String beanClassName = beanDefinition.getBeanClassName();
+            try {
+                Class<?> beanClass = Class.forName(beanClassName);
+                DeprecatedClass annotation = beanClass.getAnnotation(DeprecatedClass.class);
+                if (annotation != null) {
+                    beanDefinition.setBeanClassName(annotation.newImpl().getName());
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
